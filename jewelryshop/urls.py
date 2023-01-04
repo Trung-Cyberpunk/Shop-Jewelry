6c6c3c5291af7014b071c3be58caf3c7bc51fbfb
Nguyễn Thanh Trung <88233949+Trung-Cyberpunk@users.noreply.github.com>
2023-01-04 16:05:27 +0700

    Update urls.py

--- jewelryshop/urls.py
+++ jewelryshop/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
-
-# To display images
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
